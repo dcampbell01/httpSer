d1f09aa79b9336c34a39994bfd74ab2436ecad63
jeffBunce <schnieffenegan@yahoo.com>
2014-04-09 05:18:57 -0700

    A lot more understanding but functions are still skeletal

--- server.c
+++ server.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -11,30 +10,71 @@
 #include <pthread.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/stat.h>
 
+
+
+// Server  Constants
 #define MAXLINE	1024
-
-
 const int backlog = 10;
+const char * serverName = "The Amazing 24-hour Server";
+const char * httpVersion = "HTTP/1.0";
+
 
 // Status Messages
 const  char * OK = "200 OK"; // Request has succeeded (GET: here's your file.  HEAD: Here's info, you don't have it cached.  DELETE: Success)
 const  char * Created = "201 Created"; // New resource was created (PUT)
-const  char * NotModified = "304 Not Modified"; // Same date on cached and server resource  (HEAD: You have it cached)
+const  char * NotModified = "304 Not Modified"; // Same date on cached and server resource  (HEAD: You have it cached.  GET: You have up-to-date copy)
 const  char * BadRequest = "400 Bad Request"; // Malformed request; throw it out
 const  char * Forbidden = "403 Forbidden"; // Resource requested is not marked world-readable (PUT&DELETE: no write permission.  GET&HEAD: no read permission)
 const  char * NotFound = "404 Not Found"; // Resource DNE (GET&HEAD&DELETE)
 
-void ReadRequest(int, char *);
-void TokenizeRequest(char *request, char *command, char* resource, char* httpVersion);
 
+
+// URI struct and functions
+struct URI
+{
+  int ID;
+  char * relativePath;
+  int contentLength;
+  time_t expirationDate;
+  time_t lastModifyDate;
+};
+URI * LoadResources(URI * files, int * getNoOfFiles);
+int SaveResources();
+
+
+// Hnadle requsets of clients
+void ReadRequest(int, char *);
+void TokenizeRequest(char *request, char *command, char *resource, char *httpVersion);
+
+// Build Response
+char * BuildResponse();
+char * BuildResponse(char * httpVersion, )
+{
+  // Status-Line      =  HTTP-Verlsion Status-Code Reason-Phrase\r\n
+  // (General-Header  =  Date   =  "Date" ":" HTTP-date
+  //                   | Pragma =  "Pragma" ":" 1#pragma-directive  = pragma-directive  = no-cache" | extension-pragma
+  // | Request-Header
+  // | Entity-Header
+  // CRLF
+  // Entity-Body
+}
+
+char * BuildStatusLine();
+char * BuildGeneralHeader(char * date);
+char * BuildResponseHeader();
+char * BuildEntityHeader(char * allowedOps, int contentLen, char * expires, char * lastModified)
+
+// HTTP functions, as defined in http://www.w3.org/Protocols/HTTP/1.0/spec.html#Server
 void GET(int, char *);
 void HEAD(int, char *);
 void PUT(int, char *);
 void DELETE(int, char *);
 
 
-
+// Function Definitions
 
 void *clientHandler(void *arg)
 {
@@ -65,19 +105,23 @@ void ReadRequest(int fd, char * request)
   
   if(strcmp(request, "GET") == 0)
     {
-      //GET(...);
+      GET(); // attempt function; returns what happened
+      BulidResponse(); // given feedback, build a response
     }
   else if(strcmp(request, "HEAD") == 0)
     {	
-      //HEAD(...);
+      HEAD(); // attempt function; returns what happened
+      BulidResponse(); // given feedback, build a response
     }
   else if(strcmp(request, "PUT") == 0)
     {	
-      //PUT(...);
+      PUT(); // attempt function; returns what happened
+      BulidResponse(); // given feedback, build a response
     }
   else if(strcmp(request, "DELETE") == 0)
     {	
-      //DELETE(...);
+      DELETE(); // attempt function; returns what happened
+      BulidResponse(); // given feedback, build a response
     }
   else
     write(fd, BadRequest, strlen(BadRequest));
@@ -113,6 +157,8 @@ void DELETE(int fd, char *toBeDeleted)
 }
 
 
+
+// Server Main
 
 int main(int argc, char *argv[])
 {
@@ -154,6 +200,8 @@ int main(int argc, char *argv[])
         return -1;
 	}
 
+
+	
 	
 	while (1) {
 		clilen = sizeof(cliaddr);
